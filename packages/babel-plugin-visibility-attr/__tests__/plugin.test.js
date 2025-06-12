a1033ae022f01fd36f0c8bd65d6bba42dd810694
Kas Elvirov <kas.elvirov@gmail.com>
2025-06-12 16:03:50 +0300

    babel-plugin-visibility-attr - add lots of other possible cases for test

--- packages/babel-plugin-visibility-attr/__tests__/plugin.test.js
+++ packages/babel-plugin-visibility-attr/__tests__/plugin.test.js
@@ -1,31 +1,13 @@
+const each = require('jest-each');
+
 const babel = require('@babel/core');
 const generator = require('@babel/generator');
 
 const plugin = require('../index');
 
-const possibleCases = [
-  ['<SomeComponent data-visible={isVisible} />'],
-  ['<SomeComponent data-visible={isVisible}></SomeComponent>'],
-  ['<SomeComponent data-visible={isVisible}>test</SomeComponent>'],
-  ['<SomeComponent data-visible={isVisible} open={true}>test</SomeComponent>'],
-  ['<SomeComponent data-visible={isVisible} open={true} {...props}>test</SomeComponent>'],
-  ['<SomeComponent data-visible={isVisible} open={true} {...props}><div>test</div></SomeComponent>'],
-]
-
-const DATA = {
-  INPUT: '<SomeComponent data-visible={condition} />',
-  OUTPUT: {
-    /**
-     * Doesn't quite beautiful what we see in "toContain"
-     * but it's what we are expecting for
-     * - condition && <SomeComponent />
-    */
-    transformed: 'condition && /*#__PURE__*/React.createElement(SomeComponent, null);',
-    ast: 'React.createElement(SomeComponent',
-    jsx: 'condition && <SomeComponent />',
-  },
-};
-
+/**
+ * Just wrapper to write less code
+*/
 const transformWrapper = (code, opts = {}, transformOpts = {}) => {
   return babel.transform(code, {
     filename: 'file.jsx',
@@ -34,36 +16,103 @@ const transformWrapper = (code, opts = {}, transformOpts = {}) => {
     configFile: false,
     ...transformOpts,
   });
-}
+};
+
+/**
+ * Normalization of strings that differ slightly in formatting but are semantically identical
+*/
+const normalize = str => {
+  return str.replace(/\s+/g, '');
+};
+
+/**
+ * # All possible cases
+ *
+ * Transformed cases comment:
+ * - outputs for then are Doesn't quite beautiful what we see in "toContain"
+ * but it's what we are expecting for
+ * - condition && <SomeComponent />
+*/
+const POSSIBLE_CASES = [
+  {
+    input: '<SomeComponent data-visible={condition} />',
+    output: {
+      transformed: 'condition && /*#__PURE__*/React.createElement(SomeComponent, null);',
+      ast: 'React.createElement(SomeComponent',
+      jsx: 'condition && <SomeComponent />',
+    },
+  },
+  {
+    input: '<SomeComponent data-visible={condition}></SomeComponent>',
+    output: {
+      transformed: 'condition && /*#__PURE__*/React.createElement(SomeComponent, null);',
+      ast: 'React.createElement(SomeComponent',
+      jsx: 'condition && <SomeComponent></SomeComponent>',
+    },
+  },
+  {
+    input: '<SomeComponent data-visible={condition}>test</SomeComponent>',
+    output: {
+      transformed: 'condition && /*#__PURE__*/React.createElement(SomeComponent, null, "test");',
+      ast: 'React.createElement(SomeComponent',
+      jsx: 'condition && <SomeComponent>test</SomeComponent>',
+    },
+  },
+  {
+    input: '<SomeComponent data-visible={condition} open={true}>test</SomeComponent>',
+    output: {
+      transformed: 'condition && /*#__PURE__*/React.createElement(SomeComponent, { open: true }, "test");',
+      ast: 'React.createElement(SomeComponent',
+      jsx: 'condition && <SomeComponent open={true}>test</SomeComponent>',
+    },
+  },
+  {
+    input: '<SomeComponent data-visible={condition} open={true} {...props}>test</SomeComponent>',
+    output: {
+      transformed: 'condition && /*#__PURE__*/React.createElement(SomeComponent, _extends({ open: true }, props), "test");',
+      ast: 'React.createElement(SomeComponent',
+      jsx: 'condition && <SomeComponent open={true} {...props}>test</SomeComponent>',
+    },
+  },
+  {
+    input: '<SomeComponent data-visible={condition} open={true} {...props}><div>test</div></SomeComponent>',
+    output: {
+      transformed: 'condition && /*#__PURE__*/React.createElement(SomeComponent, _extends({ open: true }, props), /*#__PURE__*/React.createElement("div",null,"test"));',
+      ast: 'React.createElement(SomeComponent',
+      jsx: 'condition && <SomeComponent open={true} {...props}><div>test</div></SomeComponent>',
+    },
+  },
+];
 
 describe('babel-plugin-visibility-attr', () => {
-  it('main test (with react transformation)', () => {
-    const output = transformWrapper(DATA.INPUT).code;
+  each.default(POSSIBLE_CASES).describe('when transforming %s', (testCase) => {
+    it('should match transformed output', () => {
+      const result = transformWrapper(testCase.input).code;
 
-    expect(output).toContain(DATA.OUTPUT.transformed);
-  });
-
-  it('main test (snapshot)', () => {
-    const result = transformWrapper(DATA.INPUT).code;
-
-    expect(result).toMatchSnapshot();
-  });
-
-  it('ast test', () => {
-    const output = transformWrapper(DATA.INPUT, {}, { ast: true }).ast;
-
-    const printed = generator.generate(output).code;
-
-    expect(printed).toContain(DATA.OUTPUT.ast);
-  });
-
-  it('jsx test', () => {
-    const result = babel.transformSync(DATA.INPUT, {
-      plugins: [["@babel/plugin-syntax-jsx", {}], [plugin, {}]],
-      configFile: false,
-      ast: false,
+      expect(normalize(result)).toContain(normalize(testCase.output.transformed));
     });
 
-    expect(result.code).toContain(DATA.OUTPUT.jsx);
+    it('should match AST structure', () => {
+      const astResult = transformWrapper(testCase.input, {}, { ast: true }).ast;
+      const printed = generator.generate(astResult).code;
+
+      expect(printed).toContain(testCase.output.ast);
+    });
+
+    it('should preserve JSX intention', () => {
+      const result = babel.transformSync(testCase.input, {
+        plugins: [["@babel/plugin-syntax-jsx"], [plugin, {}]],
+        configFile: false,
+        ast: false,
+      });
+
+      expect(result.code).toContain(testCase.output.jsx);
+    });
+
+    it('should match snapshot', () => {
+      const result = transformWrapper(testCase.input).code;
+
+      expect(result).toMatchSnapshot();
+    });
   });
 });
